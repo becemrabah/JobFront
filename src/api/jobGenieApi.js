7f7f40222e036a44ce437d3b9b930c0ae21a784b
bassem <bassem.rabah@lrt-automotive.de>
2025-11-11 15:39:46 +0100

    test

--- src/api/jobGenieApi.js
+++ src/api/jobGenieApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
-
+console.log("API URL used:", API_URL);
 export const generateCoverLetter = async (jobDescription, cvText) => {
   const formData = new FormData();
   formData.append("job_description", jobDescription);
